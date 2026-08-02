315fbc38a7b341e81fca31c452625e752b8b9350
bradle254 <leebrad341@gmail.com>
2026-08-02 22:09:02 +0300

    hjk

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,6 @@
 import type { MetadataRoute } from "next";
 import { site } from "@/lib/site";
+export const dynamic = "force-static";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const routes = [
