fda195d9e280eba4378b6f3c7a62fcff83ca9d5a
bradle254 <leebrad341@gmail.com>
2026-08-04 12:59:48 +0300

    scroll

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: "/website",
-  assetPrefix: "/website",
 };
 
 export default nextConfig;
